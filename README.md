3874038ba6688cc1cbc6697e4d802d5866072306
elliot-hacks <thisguyhack@gmail.com>
2023-08-22 14:06:01 +0300

    Changes on caesar.py to take arguments

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # PyCryptography
+A personal project for offline cryptography tools
+
+
+To do list:
+1. **Caesar Cipher**: A simple substitution cipher where each letter in the plaintext is shifted a certain number of places down or up the alphabet.
+
+2. **Vigenère Cipher**: A more advanced substitution cipher that uses a keyword to determine the shifting amount for each letter.
+
+3. **Transposition Cipher**: Involves rearranging the letters in the plaintext to form the ciphertext.
+
+4. **Substitution Cipher**: Involves replacing each letter in the plaintext with a corresponding letter or symbol in the ciphertext.
+
+5. **RSA Encryption**: A public-key cryptosystem that involves generating a public and private key pair for secure communication.
+
+6. **AES Encryption**: A symmetric-key encryption algorithm used to secure sensitive data.
+
+7. **Hill Cipher**: A polygraphic substitution cipher that uses linear algebra for encryption and decryption.
+
+8. **Diffie-Hellman Key Exchange**: A method for two parties to agree on a shared secret key over an unsecured communication channel.
+
+9. **SHA-256 Hashing**: A cryptographic hash function that produces a fixed-size output (256 bits) from an input.
+
+10. **Simple XOR Encryption**: An XOR-based encryption method that involves bitwise operations.
+
+11. **One-Time Pad**: A theoretically unbreakable encryption method that uses a random key as long as the message.
+
+12. **Playfair Cipher**: A digraphic substitution cipher using a 5x5 matrix of letters.
+
+13. **Rail Fence Cipher**: A transposition cipher that rearranges the letters in a zigzag pattern.
+
+14. **Autokey Cipher**: A substitution cipher that uses the plaintext itself to generate part of the key for encryption.
+
+15. **Columnar Transposition Cipher**: A transposition cipher where the columns are rearranged based on a keyword.
--- caesar.py
+++ caesar.py
@@ -1,30 +1,56 @@
+import argparse
 import pyperclip
-message=input("Enter a message: ")
-key=int(input("Enter a key: "))
-mode=input("Enter a mode\n(a) encrypt\n(b) decrypt\n\t ")
-
 
 def caesarCipher(message, key, mode):
-	LETTERS='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	translated=''
-	message=message.upper()
-	
-	for letter in message:
-		if letter in LETTERS:
-			num=LETTERS.find(letter)
-			if mode=='encrypt' or mode=='a' or mode=='(a)':
-				num+=key
-			elif mode=='decrypt' or mode=='b' or mode=='(b)':
-				num-=key
-			if num>=len(LETTERS):
-				num-=len(LETTERS)
-			elif num<0:
-				num+=len(LETTERS)
-			translated+=LETTERS[num]
-		else:
-			translated+=letter
-	print(translated)
-	pyperclip.copy(translated)
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    translated = ''
+    message = message.upper()
 
+    for letter in message:
+        if letter in LETTERS:
+            num = LETTERS.find(letter)
+            if mode == 'encrypt':
+                num += key
+            elif mode == 'decrypt':
+                num -= key
+            if num >= len(LETTERS):
+                num -= len(LETTERS)
+            elif num < 0:
+                num += len(LETTERS)
+            translated += LETTERS[num]
+        else:
+            translated += letter
+    return translated
 
-caesarCipher(message, key,  mode)
+def main():
+    parser = argparse.ArgumentParser(description="Caesar Cipher Encryptor/Decryptor")
+    parser.add_argument("-e", "--encrypt", action="store_true", help="Encrypt the message")
+    parser.add_argument("-d", "--decrypt", action="store_true", help="Decrypt the message")
+    parser.add_argument("-k", "--key", type=int, help="Encryption/Decryption key")
+    parser.add_argument("-m", "--message", type=str, help="Message to encrypt/decrypt")
+
+    args = parser.parse_args()
+
+    if args.encrypt:
+        if args.key is None or args.message is None:
+            print("Both key and message are required for encryption.")
+            return
+        result = caesarCipher(args.message, args.key, 'encrypt')
+        print("Encrypted Message:")
+        print(result)
+        pyperclip.copy(result)
+
+    elif args.decrypt:
+        if args.key is None or args.message is None:
+            print("Both key and message are required for decryption.")
+            return
+        result = caesarCipher(args.message, args.key, 'decrypt')
+        print("Decrypted Message:")
+        print(result)
+        pyperclip.copy(result)
+
+    else:
+        print("Please provide either -e for encryption or -d for decryption.")
+
+if __name__ == "__main__":
+    main()
